ded59f32f7e51bcb4fac16e76a53b2da7b93201e
Dennis Jutemark <djutemark@gmail.com>
2025-02-26 15:13:41 +0100

    Add (side)scrolloff

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
 require("djutemark")
--- lua/djutemark/init.lua
+++ lua/djutemark/init.lua
@@ -1,9 +1,9 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require("djutemark.plugins")
 require("djutemark.lsp")
 require("djutemark.remap")
-
--- Set highlight on search
-vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
@@ -29,6 +29,16 @@ vim.wo.signcolumn = 'yes'
 
 -- Other
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true }) -- Keymaps for better default experience
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 16
+
+-- Set highlight on search
+vim.o.hlsearch = false
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
